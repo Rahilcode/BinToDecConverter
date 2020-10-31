c0f8617b078bbac0e92081abe36a83e93c48bf96
Rahilcode <raheelkhatttal@gmail.com>
2020-10-31 09:56:24 +0530

    Added script.js and style.css

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector('button').disabled = true;
+
+        document.getElementById('btd').onkeyup = () => {
+            if (document.getElementById('btd').value.length > 0) {
+                document.querySelector('button').disabled = false;
+            }else{
+                document.querySelector('button').disabled = true;
+            }
+        }
+
+        function convert(){
+            let val = document.getElementById('btd').value;
+            result = parseInt(val, 2).toString(10);
+            document.getElementById('dec').innerHTML = `The Decimal value is: ${result}`;
+            document.getElementById('btd').value = '';
+            document.querySelector('button').disabled = true;
+        }
+        
